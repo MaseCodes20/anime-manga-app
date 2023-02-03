45d85325f5d8d19b3a68d0840e7db756c4c0c704
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-02-02 19:08:30 -0500

    Code refactored: SearchResults component made it more reusable

--- src/Pages/SearchResultsPage.tsx
+++ src/Pages/SearchResultsPage.tsx
@@ -1,10 +1,7 @@
 import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
 import { fetchAnimesByName, fetchMangasByName } from "../api/anime";
-import Card from "../components/Card";
 import SearchResults from "../components/search/SearchResults";
-import { Anime } from "../types/animeType";
-import { Manga } from "../types/mangaType";
 
 const SearchResultsPage = () => {
   const serachTerm = useLocation().pathname.split("/")[2];
@@ -21,19 +18,9 @@ const SearchResultsPage = () => {
 
   return (
     <div>
-      {animeSuccess && (
-        <div className="bg-white/75 shadow-md rounded-md m-5 p-5">
-          <h3 className="mb-3 text-xl font-bold text-center">Animes</h3>
-          <SearchResults contents={animes} />
-        </div>
-      )}
+      {animeSuccess && <SearchResults contents={animes} title="Animes" />}
 
-      {mangaSuccess && (
-        <div className="bg-white/75 shadow-md rounded-md m-5 p-5">
-          <h3 className="mb-3 text-xl font-bold text-center">Mangas</h3>
-          <SearchResults contents={mangas} />
-        </div>
-      )}
+      {mangaSuccess && <SearchResults contents={mangas} title="Mangas" />}
     </div>
   );
 };
--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,21 +1,29 @@
-import React from "react";
 import { Anime } from "../../types/animeType";
 import { Manga } from "../../types/mangaType";
 import Card from "../Card";
 
-const SearchResults = ({ contents }: { contents: Anime[] | Manga[] }) => {
+const SearchResults = ({
+  contents,
+  title,
+}: {
+  contents: Anime[] | Manga[];
+  title: string;
+}) => {
   return (
-    <div className="grid grid-cols-8 gap-2">
-      {contents.map((content: Anime | Manga) => {
-        const contentType = content?.type !== "Manga" ? "anime" : "manga";
-        return (
-          <Card
-            key={content.mal_id}
-            content={content}
-            contentType={contentType}
-          />
-        );
-      })}
+    <div className="bg-white/75 shadow-md rounded-md m-5 p-5">
+      <h3 className="mb-3 text-xl font-bold text-center">{title}</h3>
+      <div className="grid grid-cols-8 gap-2">
+        {contents.map((content: Anime | Manga) => {
+          const contentType = content?.type !== "Manga" ? "anime" : "manga";
+          return (
+            <Card
+              key={content.mal_id}
+              content={content}
+              contentType={contentType}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
